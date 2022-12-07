f8495aa20f6a063e140df0d1aacd7a95ea00af5a
anna-raquel <agroanna.raquel@gmail.com>
2022-12-07 20:22:25 -0300

    estilo tela inicial

--- resources/views/site/layouts/basico.blade.php
+++ resources/views/site/layouts/basico.blade.php
@@ -3,9 +3,9 @@
     <head>
         <title>Spotflix - @yield('titulo')</title>
         <meta charset="utf-8">
+        <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="{{asset('css/estilo_basico.css')}}">
     </head>
-
     <body>
         @yield('conteudo')
     </body>
--- resources/views/site/principal.blade.php
+++ resources/views/site/principal.blade.php
@@ -3,14 +3,22 @@
 @section('titulo', $titulo)
 
 @section('conteudo')
-    <br>
-    <div id='div-conteudo-pagina-principal'>
-        
-        <h1>Página principal</h1>
-        <a href={{ route('site.login') }}>Login</a>
-        <a href={{ route('site.cadastro') }}>Cadastre-se</a><br><br><br>
-        <img src='img/imagemHome.jpg' class='img_primaria'>
 
+    <div id='corpo'>
+        <div id='img-fundo'>
+            <div id='div-conteudo-pagina-principal'>
+                <div id='titulo'>As melhores recomendações <br>
+                    você encontra aqui</div>
+
+                    <p id='p'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+
+                <div id='texto-btn'>
+                <a id='btn-line' href={{ route('site.cadastro') }}>Novo por aqui? <span id='span'>Cadastre-se</span> </a>
+                <a id='btn'href={{ route('site.login') }}>Entrar</a>
+
+                </div>
+
+            </div>
+        </div>
     </div>
-
 @endsection
