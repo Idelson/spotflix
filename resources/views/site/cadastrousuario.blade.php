99d56f92246d13d997916a9e24bea1b98aaab76e
anna-raquel <agroanna.raquel@gmail.com>
2022-12-07 21:43:59 -0300

    tela de cadastro

--- resources/views/site/cadastrousuario.blade.php
+++ resources/views/site/cadastrousuario.blade.php
@@ -3,23 +3,30 @@
 @section('titulo', $titulo)
 
 @section('conteudo')
-    <br><br><br><br>
-    
+
     <div class='div_formulario'>
-    
-    <h1 id='cad_usuario'>Cadastro de Usuário</h1>
+
+    <div id='cad_usuario'>Spotflix</div>
+    <p id='sub-title'>Inscreva-se grátis!</p>
         <form action={{ route('site.cadastro') }} method="post" ><!--//caso não funcione, csrf_field(), entre 2 chaves-->
             @csrf
-            <input name='name' value='{{ old('name') }}' type='text' placeholder='Usuário' class='input_form_login'><br><br>
-            
-            <input name='email' value='{{ old('email') }}' type='email' placeholder='Email' class='input_form_login'><br><br>
 
-            <input name='password' value='{{ old('password') }}' type='password' placeholder='Senha' class='input_form_login'><br><br>
-            
-            <button type='submit' class='bot'>Cadastrar</button>
-            
-        </form><br>
-        <a href='{{route('site.principal')}}'>Voltar</a>
+            <div id='form-input'>
+            <label>Usuário</label>
+            <input name='name' value='{{ old('name') }}' type='text' class='input_form_login'>
+            </div>
+            <div id='form-input'>
+            <label>Email</label>
+            <input name='email' value='{{ old('email') }}' type='email' class='input_form_login'>
+            </div>
+            <div id='form-input'>
+            <label>Senha</label>
+            <input name='password' value='{{ old('password') }}' type='password' class='input_form_login'>
+            </div>
+            <button type='submit' class='bot'>INCREVER-SE</button>
+
+        </form>
+        <a id='btn-line' href='{{route('site.principal')}}'>Voltar</a>
     </div>
-    
+
 @endsection
--- resources/views/site/principal.blade.php
+++ resources/views/site/principal.blade.php
@@ -10,7 +10,7 @@
                 <div id='titulo'>As melhores recomendações <br>
                     você encontra aqui</div>
 
-                    <p id='p'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+                <p id='p'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
 
                 <div id='texto-btn'>
                 <a id='btn-line' href={{ route('site.cadastro') }}>Novo por aqui? <span id='span'>Cadastre-se</span> </a>
