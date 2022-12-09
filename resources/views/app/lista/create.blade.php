d6474f1b006c682f6dde0cd7561aa8239ebe6efe
anna-raquel <agroanna.raquel@gmail.com>
2022-12-09 16:59:18 -0300

    criar-lista-page

--- resources/views/app/lista/create.blade.php
+++ resources/views/app/lista/create.blade.php
@@ -3,19 +3,33 @@
 @section('titulo', 'Criar Lista')
 
 @section('conteudo')
-    
-    <div id="cadastro-lista">
 
-        <form method="post" action="{{ route('lista.store') }}" enctype='multipart/form-data'>
-            @csrf
-            <input class="input-forms" name="nome" placeholder="Nome da Lista"><br><br>
-            <label for="imagem">Imagem da Lista:</label>
-            <input class="input-forms" type='file' id='imagem' name="imagem"><br><br>
 
-            <button type='submit' >Cadastrar</button>
-        </form>
-    
-    </div>
+<div id="cabecalho-padrao-criar-lista">
+<p id='texto-cabe'>Não perca tempo {{$_SESSION['name']}}, <br> mostre seus gostos para o mundo.</p>
+
+</div>
+
+<div id="cadastro-lista">
+
+    <form method="post" action="{{ route('lista.store') }}" enctype='multipart/form-data'>
+        @csrf
+        <div class="form-criate">
+            <div class="form01">
+                <label>Nome da sua lista</label>
+                <input class="input-forms" name="nome" placeholder="Nome da Lista">
+            </div>
+            <div class="form01">
+                <label for="imagem">Imagem da Lista:</label>
+                <input class="arquivo" type='file' id='imagem' name="imagem">
+            </div>
+            <div class="botao-cadastrar">
+                <button id='btn-cadastro' type='submit'>Cadastrar</button>
+            </div>
+    </form>
+
+</div>
+</div>
 
 
 
